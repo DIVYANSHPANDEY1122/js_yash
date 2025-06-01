54992664f44961d85756fae8a77aba34ed8df9a4
Divyansh pandey <yashpandey7218@gmail.com>
2025-06-01 15:44:09 +0000

    basic

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,5 @@
+"use strict";//treat all js code as newer version...
+console.log(typeof age)//string
+console.log(typeof undefined)//undefined
+//NULL GIVES OBJECT TYPE MTLB NULL EK OBJECT HAI
+console.log(typeof null);//object
---
+++ 01_basic/test.js
@@ -0,0 +1,8 @@
+// const a=100;
+// const b=200;
+// console.log(a+b)
+
+// note .......
+/* prefer not to use var 
+beacause of issue in block scope and functional scope
+*/
